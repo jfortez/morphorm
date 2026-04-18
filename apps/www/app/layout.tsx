d0c229bbfc4e928b23b81a89cda31b6df3648012
Jean Caiza <jeanmcm@hotmail.com>
2026-04-18 16:55:17 -0500

    fix: remove ts-expect

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 
-// @ts-expect-error nah it's fine
 import "morphorm/styles.css";
-// @ts-expect-error nah it's fine
 import "./globals.css";
 
 const geistSans = Geist({
